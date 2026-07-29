3b654eed3ddbc38864e480c46018a4a504770a7f
wertos97 <wilkmati@gmail.com>
2026-07-29 13:19:33 +0200

    ciagle poprawiam

--- server.py
+++ server.py
@@ -778,7 +778,9 @@ class MPKRequestHandler(SimpleHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-Type', 'text/html; charset=utf-8')
             self.send_header('Content-Length', str(len(body)))
-            self.send_header('Cache-Control', 'no-cache')
+            self.send_header('Cache-Control', 'no-cache, no-store, must-revalidate')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Expires', '0')
             self.end_headers()
             self.wfile.write(body)
             
