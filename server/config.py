b34a624355e7a6467bb8561c73e3b327de6dc451
wertos97 <wilkmati@gmail.com>
2026-09-01 10:12:13 +0200

    1.4.7

--- server/config.py
+++ server/config.py
@@ -156,4 +156,4 @@ PRICING_PATH = os.path.join(BASE_DIR, 'pricing.json')
 # ============================================================
 # Application Version
 # ============================================================
-APP_VERSION = "1.4.6"
+APP_VERSION = "1.4.7"
